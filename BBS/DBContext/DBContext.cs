51e33a5426edc48d869803d2f5dc54f29b19e9cf
crazyoung <yangfeng666@sina.cn>
2018-09-05 07:52:02 +0800

    增加entity

---
+++ BBS/DBContext/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBS.DBContext
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() { }
+    }
+}
---
+++ BBS/Models/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBS.Models.Role
+{
+    public class Entity
+    {
+        [Key]
+        [Required]
+        [Column("ID", TypeName = "int")]
+        public int ID { get; set; }
+        [Column("CreateTime", TypeName = "nvarchar")]
+        public string CreateDate { get; set; }
+        [Column("LoginTime", TypeName = "nvarchar")]
+        public string LoginDate { get; set; }
+    }
+}

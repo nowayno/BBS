0b1af1dd8b746127861f5c35812283882886e722
crazyoung <yangfeng666@sina.cn>
2018-09-06 00:10:45 +0800

    增加新的模型类

---
+++ BBS/Migrations/20180905143939_initial.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BBS.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "role",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateTime = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    LoginTime = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    role_name = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    pass = table.Column<int>(type: "int", nullable: false),
+                    reg_name = table.Column<string>(type: "nvarchar(20)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role_info",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateTime = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    LoginTime = table.Column<string>(type: "nvarchar(20)", nullable: true),
+                    sex = table.Column<int>(type: "int", nullable: false),
+                    lvl = table.Column<int>(type: "int", nullable: false),
+                    phone = table.Column<string>(type: "nvarchar(13)", nullable: false),
+                    rid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role_info", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_role_info_role_rid",
+                        column: x => x.rid,
+                        principalTable: "role",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_role_info_rid",
+                table: "role_info",
+                column: "rid",
+                unique: true,
+                filter: "[rid] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "role_info");
+
+            migrationBuilder.DropTable(
+                name: "role");
+        }
+    }
+}
---
+++ BBS/Models/Roles/RoleInfo.cs
@@ -0,0 +1,25 @@
+﻿using BBS.Models.Role;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBS.Models.Roles
+{
+    [Table("role_info")]
+    public class RoleInfo : Entity
+    {
+        [Column("sex", TypeName = "int")]
+        public int Sex { get; set; }
+        [Column("lvl", TypeName = "int")]
+        public int Level { get; set; }
+        [Column("phone", TypeName = "nvarchar(13)")]
+        public int Phone { get; set; }
+        [ForeignKey("rid")]
+        public Role Rid { get; set; }
+        //public int Rid { get; set; }
+    }
+}
